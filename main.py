b9f657a97913dff41f9932928a1ae8c29afe1028
kaden <kadenfranklin@hotmail.com>
2022-01-30 13:43:15 -0600

    successfully prints data from excel

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from pandas import DataFrame
-from selenium import webdriver
 import pandas as pd
+from selenium import webdriver
 
 data = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='1-1150')
 # df = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='3000-5000')
-df = df = pd.DataFrame(data, columns= ['ID','Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ', 'Target', 'Defects', 'Cond', 'Maintenance', '???', 'Comments', 'Planted', 'Icon', 'No.' ])
-for i in df:
-    print(df)
-browser = webdriver.Chrome()
+df = df = pd.DataFrame(data, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'Ht', 'HAZ ', 'Target', 'Defects', 'Cond.', 'Maintenance', '???', 'Comments'])
+# imports columns^ the loop through rows
+for i in df.iterrows():
+    print(i)
+    
+# browser = webdriver.Chrome()
 # Need to fill in specialties & scientific categories before making a tree
 # (Check IF data column for that tree matches df)
 # browser.get('https://localhost:44367/Trees/Create')
