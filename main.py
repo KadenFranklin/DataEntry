9aea9e7179a27c52b909c9a0f1585e0f595c40af
kaden <kadenfranklin@hotmail.com>
2022-02-01 20:16:22 -0600

    deez

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 data1 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='1-1150')
 df = pd.DataFrame(data1, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'Ht', 'HAZ ', 'Target', 'Defects', 'Cond.', 'Maintenance', '???', 'Comments'])
@@ -7,13 +9,22 @@ df = pd.DataFrame(data1, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Co
 # data2 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='3000-5000')
 # df2 = pd.DataFrame(data2, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'Ht', 'HAZ ', 'Target', 'Defects', 'Cond.', 'Maintenance', '???', 'Comments', 'Planted', 'Photo', 'Icon', 'No.'])
 
-# imports columns^ the loop through rows
+# getitem() separates the index from the series
+
+browser = webdriver.Chrome()
+browser.get('https://localhost:44367/Trees/Create')
+
 for i in df.iterrows():
-    print(i)
-    
-# browser = webdriver.Chrome()
+    dat_list = i.__getitem__(1)
+    dis = dat_list[0]
+    browser.find_element(By.TAG_NAME, "input").below({By.ID: "TreeID"}).send_keys(dis)
+    print(dis)
+    if dis == 1:
+        break
+
+
 # Need to fill in specialties & scientific categories before making a tree
 # (Check IF data column for that tree matches df)
-# browser.get('https://localhost:44367/Trees/Create')
+#
 # browser.get('https://localhost:44367/Scientifics/Create')
 # browser.get('https://localhost:44367/Specialties')
