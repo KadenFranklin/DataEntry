2bd93da3cf8b4a408c629fdc1e7498b4f8eb6bc8
kaden <kadenfranklin@hotmail.com>
2022-02-03 01:28:08 -0600

    fix nan and good to go

--- main.py
+++ main.py
@@ -5,30 +5,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
-data1 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='1-1150')
-df = pd.DataFrame(data1, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
 
 # data2 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='3000-5000')
-# df2 = pd.DataFrame(data2, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'Ht', 'HAZ ', 'Target', 'Defects', 'Cond.', 'Maintenance', '???', 'Comments', 'Planted', 'Photo', 'Icon', 'No.'])
+# df = pd.DataFrame(data2, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
 
+data1 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='1-1150')
+df = pd.DataFrame(data1, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
 browser = webdriver.Chrome()
-browser.get('https://localhost:44367/Trees/Create')
 
 for i in df.iterrows():
-    
+    browser.get('https://localhost:44367/Trees/Create')
+    browser.refresh()
     dis = i.__getitem__(1)
     if (dis[1] == 'nan') & (dis[2] == 'nan') & (dis[3] == 'nan'):
         continue
     else:
         tree_id = browser.find_element(By.ID, "Tree_TreeID")
         tree_id.send_keys(dis[0])
-
         tree_lat = browser.find_element(By.ID, "Tree_Latitude")
         tree_lat.send_keys(dis[2])
-
         tree_long = browser.find_element(By.ID, "Tree_Longitude")
         tree_long.send_keys(dis[3])
-
         sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
         location = browser.find_element(By.ID, "Tree_LocationName")
         count = 0
@@ -66,29 +63,24 @@ for i in df.iterrows():
                 ARROW_DOWN = u'\ue015'
                 name.send_keys(ARROW_DOWN)
                 count += 1
-
         if dis[6] != 'nan':
             tree_dbh = browser.find_element(By.ID, "Tree_DBH")
             tree_dbh.send_keys(dis[6])
-
         if dis[7] != 'nan':
             tree_hgt = browser.find_element(By.ID, "Tree_Height")
             tree_hgt.send_keys(dis[7])
-
         if dis[8] != 'nan':
             tree_haz = browser.find_element(By.ID, "Tree_Hazard")
             tree_haz.send_keys(dis[8])
-
         if dis[9] != 'nan':
             tree_cond = browser.find_element(By.ID, "Tree_Condition")
             tree_cond.send_keys(dis[9])
-
         if dis[10] != 'nan':
             tree_com = browser.find_element(By.ID, "Tree_Comment")
             tree_com.send_keys(dis[10])
-
         enter = browser.find_element(By.ID, "ENTER")
         ActionChains(browser).click(enter).perform()
-
-        if dis[0] == 1:
+        # don't need to add a wait after the click
+        # need to fix the nan problem
+        if dis[0] == 2:
             break
