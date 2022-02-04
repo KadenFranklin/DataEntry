544092a56782e597f9335873ae23932b797fa988
kaden <kadenfranklin@hotmail.com>
2022-02-04 12:44:40 -0600

    Bug/reload fixing
    Now we ready

--- main.py
+++ main.py
@@ -28,41 +28,86 @@ for i in df.iterrows():
         tree_long.send_keys(dis[3])
         sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
         location = browser.find_element(By.ID, "Tree_LocationName")
-        count = 0
+        count = 1
+
         for option in sel_loc.options:
             if option.text == dis[1]:
                 location.send_keys(Keys.ENTER)
                 break
-            elif count == len(sel_loc.options):
+            if count == len(sel_loc.options):
                 browser.get('https://localhost:44367/Locations/Create')
                 new_loc = browser.find_element(By.ID, "Location_LocationName")
                 new_loc.send_keys(dis[1])
                 enter1 = browser.find_element(By.ID, "ENTER")
                 ActionChains(browser).click(enter1).perform()
                 browser.get('https://localhost:44367/Trees/Create')
+                tree_id = browser.find_element(By.ID, "Tree_TreeID")
+                tree_id.send_keys(dis[0])
+                tree_lat = browser.find_element(By.ID, "Tree_Latitude")
+                tree_lat.send_keys(dis[2])
+                tree_long = browser.find_element(By.ID, "Tree_Longitude")
+                tree_long.send_keys(dis[3])
+                sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
+                location = browser.find_element(By.ID, "Tree_LocationName")
+                for opt in sel_loc.options:
+                    if opt.text == dis[1]:
+                        location.send_keys(Keys.ENTER)
+                        break
+                    else:
+                        ARROW_DOWN = u'\ue015'
+                        location.send_keys(ARROW_DOWN)
             else:
                 ARROW_DOWN = u'\ue015'
                 location.send_keys(ARROW_DOWN)
                 count += 1
-
         sel_name = Select(browser.find_element(By.ID, "Tree_ScientificName"))
         name = browser.find_element(By.ID, "Tree_ScientificName")
-        count2 = 0
+        count2 = 1
+
         for option in sel_name.options:
             if option.text == dis[5]:
                 name.send_keys(Keys.ENTER)
                 break
-            elif count2 == len(sel_name.options):
-                browser.get('https://localhost:44367/Scientific/Create')
+            if count2 == len(sel_name.options):
+                browser.get('https://localhost:44367/Scientifics/Create')
                 new_name = browser.find_element(By.ID, "Scientific_ScientificName")
                 new_name.send_keys(dis[5])
+                enter1_2 = browser.find_element(By.ID, "add_common")
+                ActionChains(browser).click(enter1_2).perform()
+                common = browser.find_element(By.ID, "CommonNames[0]_Name")
+                common.send_keys(dis[4])
                 enter2 = browser.find_element(By.ID, "ENTER")
                 ActionChains(browser).click(enter2).perform()
                 browser.get('https://localhost:44367/Trees/Create')
+                tree_id = browser.find_element(By.ID, "Tree_TreeID")
+                tree_id.send_keys(dis[0])
+                tree_lat = browser.find_element(By.ID, "Tree_Latitude")
+                tree_lat.send_keys(dis[2])
+                tree_long = browser.find_element(By.ID, "Tree_Longitude")
+                tree_long.send_keys(dis[3])
+                sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
+                location = browser.find_element(By.ID, "Tree_LocationName")
+                for opt in sel_loc.options:
+                    if opt.text == dis[1]:
+                        location.send_keys(Keys.ENTER)
+                        break
+                    else:
+                        ARROW_DOWN = u'\ue015'
+                        location.send_keys(ARROW_DOWN)
+                sel_name = Select(browser.find_element(By.ID, "Tree_ScientificName"))
+                name = browser.find_element(By.ID, "Tree_ScientificName")
+                count2 = 1
+                for op in sel_name.options:
+                    if op.text == dis[5]:
+                        name.send_keys(Keys.ENTER)
+                        break
+                    else:
+                        ARROW_DOWN = u'\ue015'
+                        name.send_keys(ARROW_DOWN)
             else:
                 ARROW_DOWN = u'\ue015'
                 name.send_keys(ARROW_DOWN)
-                count += 1
+                count2 += 1
         if not isinstance(dis[6], float):
             tree_dbh = browser.find_element(By.ID, "Tree_DBH")
             tree_dbh.send_keys(dis[6])
@@ -80,3 +125,5 @@ for i in df.iterrows():
             tree_com.send_keys(dis[10])
         enter = browser.find_element(By.ID, "ENTER")
         ActionChains(browser).click(enter).perform()
+        if dis[0] == 2:
+            break
