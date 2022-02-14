07ce7a11f75b99968c16a245db4a40fcc82eef67
kaden <kadenfranklin@hotmail.com>
2022-02-14 12:39:56 -0600

    special

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import Select
 
 
 data2 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='3000-5000')
-df = pd.DataFrame(data2, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
+df = pd.DataFrame(data2, columns=['Name', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
 
 # data1 = pd.read_excel(r"C:\Users\Kaden's Laptop\PycharmProjects\DataEntry\Tree_Data.xlsx", sheet_name='1-1150')
 # df = pd.DataFrame(data1, columns=['ID', 'Location', 'Latitude', 'Longitude', 'Common name', 'Scientific name', 'DBH', 'Ht', 'HAZ ', 'Cond.', 'Comments'])
@@ -18,7 +18,7 @@ for i in df.iterrows():
     browser.get('https://localhost:44367/Trees/Create')
     browser.refresh()
     dis = i.__getitem__(1)
-    if (not isinstance(dis[1], float)) & (not isinstance(dis[2], float)) & (not isinstance(dis[3], float)):
+    if (not isinstance(dis[1], float)) | (not isinstance(dis[2], float)) | (not isinstance(dis[3], float)):
         continue
     else:
         tree_id = browser.find_element(By.ID, "Tree_TreeID")
@@ -77,52 +77,53 @@ for i in df.iterrows():
                 ActionChains(browser).click(enter1_2).perform()
                 common = browser.find_element(By.ID, "CommonNames[0]_Name")
                 common.send_keys(dis[4])
-                enter2 = browser.find_element(By.ID, "ENTER")
-                ActionChains(browser).click(enter2).perform()
-                browser.get('https://localhost:44367/Trees/Create')
-                tree_id = browser.find_element(By.ID, "Tree_TreeID")
-                tree_id.send_keys(dis[0])
-                tree_lat = browser.find_element(By.ID, "Tree_Latitude")
-                tree_lat.send_keys(dis[2])
-                tree_long = browser.find_element(By.ID, "Tree_Longitude")
-                tree_long.send_keys(dis[3])
-                sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
-                location = browser.find_element(By.ID, "Tree_LocationName")
-                for opt in sel_loc.options:
-                    if opt.text == dis[1]:
-                        location.send_keys(Keys.ENTER)
-                        break
-                    else:
-                        ARROW_DOWN = u'\ue015'
-                        location.send_keys(ARROW_DOWN)
-                sel_name = Select(browser.find_element(By.ID, "Tree_ScientificName"))
-                name = browser.find_element(By.ID, "Tree_ScientificName")
-                count2 = 1
-                for op in sel_name.options:
-                    if op.text == dis[5]:
-                        name.send_keys(Keys.ENTER)
-                        break
-                    else:
-                        ARROW_DOWN = u'\ue015'
-                        name.send_keys(ARROW_DOWN)
-            else:
-                ARROW_DOWN = u'\ue015'
-                name.send_keys(ARROW_DOWN)
-                count2 += 1
-        if not isinstance(dis[6], float):
-            tree_dbh = browser.find_element(By.ID, "Tree_DBH")
-            tree_dbh.send_keys(dis[6])
-        if not isinstance(dis[7], float):
-            tree_hgt = browser.find_element(By.ID, "Tree_Height")
-            tree_hgt.send_keys(dis[7])
-        if not isinstance(dis[8], float):
-            tree_haz = browser.find_element(By.ID, "Tree_Hazard")
-            tree_haz.send_keys(dis[8])
-        if not isinstance(dis[9], float):
-            tree_cond = browser.find_element(By.ID, "Tree_Condition")
-            tree_cond.send_keys(dis[9])
-        if not isinstance(dis[10], float):
-            tree_com = browser.find_element(By.ID, "Tree_Comment")
-            tree_com.send_keys(dis[10])
-        enter = browser.find_element(By.ID, "ENTER")
-        ActionChains(browser).click(enter).perform()
+            enter2 = browser.find_element(By.ID, "ENTER")
+            ActionChains(browser).click(enter2).perform()
+            browser.get('https://localhost:44367/Trees/Create')
+            tree_id = browser.find_element(By.ID, "Tree_TreeID")
+            tree_id.send_keys(dis[0])
+            tree_lat = browser.find_element(By.ID, "Tree_Latitude")
+            tree_lat.send_keys(dis[2])
+            tree_long = browser.find_element(By.ID, "Tree_Longitude")
+            tree_long.send_keys(dis[3])
+            sel_loc = Select(browser.find_element(By.ID, "Tree_LocationName"))
+            location = browser.find_element(By.ID, "Tree_LocationName")
+            for opt in sel_loc.options:
+                if opt.text == dis[1]:
+                    location.send_keys(Keys.ENTER)
+                    break
+                else:
+                    ARROW_DOWN = u'\ue015'
+                    location.send_keys(ARROW_DOWN)
+            sel_name = Select(browser.find_element(By.ID, "Tree_ScientificName"))
+            name = browser.find_element(By.ID, "Tree_ScientificName")
+            count2 = 1
+            for op in sel_name.options:
+                if op.text == dis[5]:
+                    name.send_keys(Keys.ENTER)
+                    break
+                else:
+                    ARROW_DOWN = u'\ue015'
+                    name.send_keys(ARROW_DOWN)
+        else:
+            ARROW_DOWN = u'\ue015'
+            name.send_keys(ARROW_DOWN)
+            count2 += 1
+    if not isinstance(dis[6], float):
+        tree_dbh = browser.find_element(By.ID, "Tree_DBH")
+        tree_dbh.send_keys(dis[6])
+    if not isinstance(dis[7], float):
+        tree_hgt = browser.find_element(By.ID, "Tree_Height")
+        tree_hgt.send_keys(dis[7])
+    if not isinstance(dis[8], float):
+        tree_haz = browser.find_element(By.ID, "Tree_Hazard")
+        tree_haz.send_keys(dis[8])
+    if not isinstance(dis[9], float):
+        tree_cond = browser.find_element(By.ID, "Tree_Condition")
+        tree_cond.send_keys(dis[9])
+    if not isinstance(dis[10], float):
+        tree_com = browser.find_element(By.ID, "Tree_Comment")
+        tree_com.send_keys(dis[10])
+    enter = browser.find_element(By.ID, "ENTER")
+    ActionChains(browser).click(enter).perform()
+
