8ea7cc0899e782544b18f9d2d4fe2c9d0abd5041
kaden <kadenfranklin@hotmail.com>
2022-02-17 10:12:46 -0600

    almost there

--- specialties.py
+++ specialties.py
@@ -13,39 +13,16 @@ from selenium.webdriver.support.ui import Select
 # Trees with Plaques (comments contain 'PLQ')
 
 browser = webdriver.Chrome()
-browser.get('https://localhost:44367/trees/')
+browser.get('https://localhost:44367/Trees/')
 browser.refresh()
+rows = browser.find_elements(By.TAG_NAME, "tr")
 
+# for every row (maybe select element by index)
+# check scientific name
+# open a new tab, & edit the trees data
 
-table_id = browser.find_element(By.TAG_NAME, 'tbody')
-rows = table_id.find_elements(By.TAG_NAME, "tr")
-for row in rows:
-    tree_id = row.find_elements(By.TAG_NAME, "td")[0]
-    comment = row.find_elements(By.TAG_NAME, "td")[7]
-    sci_name = row.find_elements(By.TAG_NAME, "td")[9]
-    crud_row = row.find_elements(By.TAG_NAME, "td")[10]
-    edit = crud_row.find_elements(By.TAG_NAME, "a")[0]
+# for category in specialties
+# choose the correct cat
 
-    new = webdriver.Chrome()
-    new.get('https://localhost:44367/Trees/Edit?id=' + tree_id.text)
-    sel_spec = Select(browser.find_element(By.ID, "SpecialtyTree_SpecialtyTitle"))
-    spec = browser.find_element(By.ID, "SpecialtyTree_SpecialtyTitle")
-    count = 1
-
-    for option in sel_spec.options:
-        # do some comparison with scientific name
-        # maybe write a function
-        if option.text == dis[1]:
-            spec.send_keys(Keys.ENTER)
-            break
-        # if count == len(sel_spec.options):
-            # if run out of options
-            # shouldnt happen
-        else:
-            ARROW_DOWN = u'\ue015'
-            spec.send_keys(ARROW_DOWN)
-            count += 1
-    # after selecting correct category,
-    # add category button
-    # maybe a refresh button
-    # save button
+# enter = browser.find_element(By.ID, "ENTER")
+# ActionChains(browser).click(enter).perform()
